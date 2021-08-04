27ce7e38ed76b01a1826a94b84917c3e8beb235f
muslimr <muslim.ragimov@yahoo.com>
2021-08-04 17:38:02 +0400

    Map has been added

--- client/src/layouts/Main/ContactsPage/components/MyMap.js
+++ client/src/layouts/Main/ContactsPage/components/MyMap.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Map, Marker } from "pigeon-maps";
+import { Map, Marker, ZoomControl } from "pigeon-maps";
 
 
 export default function MyMap() {
     return (
-        <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-            <Marker width={50} anchor={[50.879, 4.6997]}/>
+        <Map height={300} defaultCenter={[40.409904, 49.943341]} defaultZoom={17}>
+            <ZoomControl />
+            <Marker width={50} anchor={[40.409904, 49.943341]}/>
         </Map>
     );
 }
